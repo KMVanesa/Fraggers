f295139711b7c6b4cf06a25fa11dfbffa49518c9
Krutarth Vanesa <vanesa.k@husky.neu.edu>
2020-04-03 02:30:13 -0400

    Auth User are Committed

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,8 +1,17 @@
 const express=require('express');
 const router=express.Router();
-
+const auth =require('../../middleware/auth'); 
+const User=require('../../models/User');
 //@route GET api/auth
 //@access Public
-router.get('/',(req,res)=>res.send('Auth Route'));
+router.get('/',auth,async (req,res)=>{
+    try {
+        const user=await (await User.findById(req.user.id).select('-password'));
+        //res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports=router;
