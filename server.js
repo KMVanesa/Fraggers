25bef13fa6ac774498e39c3e37f998db31f05a6a
Krutarth Vanesa <vanesa.k@husky.neu.edu>
2020-04-02 23:40:48 -0400

    Config files are Committed

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express=require('express');
 const app=express();
-
+const connectDB=require('./config/db');
 
 const PORT=process.env.PORT || 5000;
+//connect DB
+connectDB();
 
+//Listening Port
 app.listen(PORT,()=>console.log(`Port: ${PORT}`));
-
 app.get('/',(req,res)=>res.send('API Running'));
