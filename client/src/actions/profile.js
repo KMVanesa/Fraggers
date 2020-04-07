2d23a835b65f67e8c61d8b658b67c07999045dff
Krutarth Vanesa <vanesa.k@husky.neu.edu>
2020-04-07 13:25:07 -0400

    Dashboard Set up completed

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert'
-import { PROFILE_ERROR, GET_PROFILE, UPDATE_PROFILE } from './types';
+import { PROFILE_ERROR, GET_PROFILE, UPDATE_PROFILE,ACCOUNT_DELETED,CLEAR_PROFILE } from './types';
 
 //Get current users profile
 export const getCurrentProfile = () => async dispatch => {
@@ -78,7 +78,7 @@ export const addExperience = (formData, history) => async dispatch => {
             payload: { msg: err.response.statusText, status: err.response.status }
         })
     }
-} 
+}
 
 
 //Add Education
@@ -109,4 +109,63 @@ export const addEducation = (formData, history) => async dispatch => {
             payload: { msg: err.response.statusText, status: err.response.status }
         })
     }
-} 
+}
+
+
+// Delete experience
+export const deleteExperience = id => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/profile/experience/${id}`);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Experience Removed', 'success'));
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+
+// Delete education
+export const deleteEducation = id => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/profile/education/${id}`);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Education Removed', 'success'));
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+
+export const deleteAccount = () => async dispatch => {
+    if (window.confirm('Are you sure? This can NOT be undone!')) {
+        try {
+            await axios.delete('/api/profile');
+
+            dispatch({ type: CLEAR_PROFILE });
+            dispatch({ type: ACCOUNT_DELETED });
+
+            dispatch(setAlert('Your account has been permanantly deleted'));
+        } catch (err) {
+            dispatch({
+                type: PROFILE_ERROR,
+                payload: { msg: err.response.statusText, status: err.response.status }
+            });
+        }
+    }
+};
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,12 +1,15 @@
 import React, { useEffect, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { getCurrentProfile } from '../../actions/profile'
+import { getCurrentProfile, deleteAccount } from '../../actions/profile'
 import Spinner from '../layouts/Spinner'
 import { Link } from 'react-router-dom'
 import DashboardActions from './DashboardActions'
+import Experience from './Experience'
+import Education from './Education'
 
-const Dashboard = ({ getCurrentProfile, auth:{user}, profile: { profile, loading } }) => {
+
+const Dashboard = ({ getCurrentProfile, auth:{user}, profile: { profile, loading } ,deleteAccount}) => {
 
     useEffect(() => {
         getCurrentProfile()
@@ -15,7 +18,10 @@ const Dashboard = ({ getCurrentProfile, auth:{user}, profile: { profile, loading
     return loading && profile === null ? <Spinner /> : <Fragment>
         <h1 className="large text-primary">Dashboard</h1>
         <p className="lead"><i className="las la-user la-lg"></i>Welcome {user && user.name}</p>
-        {profile!==null ? <Fragment><DashboardActions/></Fragment> : <Fragment>
+        {profile!==null ? <Fragment><DashboardActions/><Experience experience={profile.experience} /><Education education={profile.education} /><div className="my-2">
+            <button className="btn btn-danger" onClick={()=>deleteAccount()}><i class="las la-user-slash" ></i>Delete</button>
+        </div>
+        </Fragment> : <Fragment>
                 <p>You don't have profile, Click Here to create a one</p>
                 <Link to='/create-profile' className='btn btn-primary my-1'>Create Profile</Link>
             </Fragment>}
@@ -26,6 +32,7 @@ Dashboard.propTypes = {
     getCurrentProfile: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
     profile: PropTypes.object.isRequired,
+    deleteAccount:PropTypes.func.isRequired,
 }
 
 const mapStateToProps = state => ({
@@ -33,4 +40,4 @@ const mapStateToProps = state => ({
     profile: state.profile
 })
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile,deleteAccount })(Dashboard);
